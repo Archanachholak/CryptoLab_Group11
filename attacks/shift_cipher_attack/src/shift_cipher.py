ffa9d96290ffbeaeb0332643a9ca7d4b7468c04a
mnit <mnit@lab.local>
2026-08-19 11:57:12 +0530

    Set up Shift Cipher attack structure

---
+++ attacks/shift_cipher_attack/src/shift_cipher.py
@@ -0,0 +1,29 @@
+def encrypt(text, key):
+    result = ""
+
+    for char in text:
+        if char.isupper():
+            result += chr((ord(char) - ord('A') + key) % 26 + ord('A'))
+        elif char.islower():
+            result += chr((ord(char) - ord('a') + key) % 26 + ord('a'))
+        else:
+            result += char
+
+    return result
+
+
+def decrypt(text, key):
+    return encrypt(text, -key)
+
+
+if __name__ == "__main__":
+    plaintext = "HELLO WORLD"
+    key = 3
+
+    ciphertext = encrypt(plaintext, key)
+    decrypted = decrypt(ciphertext, key)
+
+    print("Plaintext :", plaintext)
+    print("Key       :", key)
+    print("Ciphertext:", ciphertext)
+    print("Decrypted :", decrypted)
